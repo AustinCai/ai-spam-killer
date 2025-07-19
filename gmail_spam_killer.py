ec562a7073a07a8c48086546c8cbf4960a94d835
Austin Cai <austins.cai@gmail.com>
2025-07-19 12:35:30 -0400

    update requirements dependencies

--- gmail_spam_killer.py
+++ gmail_spam_killer.py
@@ -165,7 +165,7 @@ class GmailSpamKiller:
         
         try:
             response = self.openai_client.chat.completions.create(
-                model="gpt-3.5-turbo",
+                model="gpt-4.1",
                 messages=[{"role": "user", "content": prompt}],
                 max_tokens=100,
                 temperature=0.1
